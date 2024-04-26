05bcc3d196a52e3d483c6f0a8fcf135273060073
Richardas Kuchinskas <r.kuchinskas@vk.team>
2024-04-26 05:26:54 +0300

    prototype

---
+++ LRU_Cache.cpp
@@ -0,0 +1,83 @@
+#include "LRU_Cache.h"
+
+LRU_Cache::LRU_Cache(size_t capacity) : capacity_(capacity) {
+
+}
+
+size_t LRU_Cache::size() const {
+    return size_;
+}
+
+void LRU_Cache::put(std::string &key, int value) {
+    auto it = accessStorage_.find(key);
+    if (it != accessStorage_.end()) {
+        it->second->value = value;
+        itemList_.splice(itemList_.begin(), itemList_, it->second);
+    } else {
+        if (size_ >= capacity_) {
+            accessStorage_.erase(itemList_.back().key);
+            itemList_.pop_back();
+            --size_;
+        }
+    }
+
+    itemList_.emplace_front(key, value);
+    accessStorage_[key] = itemList_.begin();
+    ++size_;
+}
+
+bool LRU_Cache::isEmpty() const {
+    return size_ == 0;
+}
+
+void LRU_Cache::clear() {
+    itemList_.clear();
+    accessStorage_.clear();
+    size_ = 0;
+}
+
+LRU_Cache::LRU_Cache(const LRU_Cache &other) : size_(other.size_), capacity_(other.capacity_) {
+    itemList_ = other.itemList_;
+
+    auto it = itemList_.begin();
+
+    while (it != itemList_.end()) {
+        accessStorage_[it->key] = it;
+        ++it;
+    }
+}
+
+LRU_Cache &LRU_Cache::operator=(const LRU_Cache &other) {
+    if (this != &other) {
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        itemList_.clear();
+        accessStorage_.clear();
+        for (const auto &item: other.itemList_) {
+            itemList_.push_back(item);
+        }
+        for (auto it = itemList_.begin(); it != itemList_.end(); ++it) {
+            accessStorage_[it->key] = it;
+        }
+    }
+    return *this;
+}
+
+LRU_Cache::LRU_Cache(LRU_Cache &&other) noexcept : size_(other.size_), capacity_(other.capacity_), itemList_(std::move(other.itemList_)), accessStorage_(std::move(other.accessStorage_)){
+    other.size_ = 0;
+}
+
+LRU_Cache& LRU_Cache::operator=(LRU_Cache &&other) noexcept{
+    if(this != &other){
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        itemList_ = std::move(other.itemList_);
+        accessStorage_ = std::move(other.accessStorage_);
+        other.size_ = 0;
+    }
+    return *this;
+}
+
+LRU_Cache::~LRU_Cache() {
+
+}
---
+++ LRU_Cache.h
@@ -0,0 +1,59 @@
+#ifndef LRU_CACHE_LRU_CACHE_H
+#define LRU_CACHE_LRU_CACHE_H
+
+#include <unordered_map>
+#include <string>
+#include <list>
+#include <iterator>
+#include <utility>
+
+class LRU_Cache {
+public:
+    LRU_Cache() = delete;
+
+    explicit LRU_Cache(size_t capacity);
+
+    LRU_Cache(const LRU_Cache &other);
+
+    LRU_Cache& operator=(const LRU_Cache &other);
+
+    LRU_Cache(LRU_Cache &&other) noexcept;
+
+    LRU_Cache& operator=(LRU_Cache &&other) noexcept;
+
+    ~LRU_Cache();
+
+    bool isEmpty() const;
+
+    size_t size() const;
+
+    void clear();
+
+    void put(std::string &key, int value);
+
+private:
+    struct CacheItemNode {
+    public:
+        std::string key;
+        int value = 0;
+
+       explicit CacheItemNode(std::string key, int value) : key(std::move(key)), value(value) {}
+    };
+
+    std::unordered_map<std::string, std::list<CacheItemNode>::iterator> accessStorage_;
+    std::list<CacheItemNode> itemList_;
+    size_t capacity_ = 0;
+    size_t size_ = 0;
+
+
+    class Iterator {
+    public:
+
+    private:
+        using list_iterator = std::list<CacheItemNode>::iterator;
+        list_iterator it;
+    };
+};
+
+
+#endif //LRU_CACHE_LRU_CACHE_H
